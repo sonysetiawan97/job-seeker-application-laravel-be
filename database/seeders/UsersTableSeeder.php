7f38b9e48123452649116ae0ccacfc32482566d5
Sony Setiawan <sonysetiawanred@gmail.com>
2022-05-23 12:29:50 +0700

    update seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -26,11 +26,12 @@ class UsersTableSeeder extends Seeder
                     'status' => 'active',
                     'email_verified_at' => date('Y-m-d H:i:s'),
                     'religion' => 'hidden',
-                    'phone' => '0857xxxxxxxx',
+                    'phone' => '02179170400',
                     'country_id' => 1,
                     'province_id' => 11,
                     'city_id' => 155,
-                    'residence' => 'Jl. Mampang Prapatan Raya 108 Rukan Buncit Mas, Blok C3A, Lantai 2-4, Daerah Khusus Ibukota Jakarta 12760'
+                    'residence' => 'Jl. Mampang Prapatan Raya 108 Rukan Buncit Mas, Blok C3A, Lantai 2-4, Daerah Khusus Ibukota Jakarta 12760',
+                    'dob' => '2010-01-01',
                 ],
             ];
 
