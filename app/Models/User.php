af0796836aaa2f3b4916bfd30113b56d873fcecc
Sony Setiawan <sonysetiawanred@gmail.com>
2022-05-24 15:20:02 +0700

    feat: find company on user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Traits\HasRoles;
 use Laravel\Passport\HasApiTokens;
 
@@ -43,4 +44,18 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function company($id)
+    {
+        $company = DB::table('users')
+            ->join('user_company', 'user_id', '=', 'users.id')
+            ->join('companies', 'companies.id', '=', 'user_company.company_id')
+            ->select('companies.*')
+            ->whereNull('user_company.deleted_at')
+            ->where('users.id', '=', $id)
+            ->orderBy('user_company.updated_at', 'desc')
+            ->first();
+
+        return $company;
+    }
 }
